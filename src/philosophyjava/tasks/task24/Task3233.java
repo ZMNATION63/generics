01113958dd6a85590bea1c94e37571e2f1c5bc54
Misha Zaynutdinov <mzaynutdinov@yandex.ru>
2022-06-06 14:55:02 +0400

    MultiplelnterfaceVariants

---
+++ src/philosophyjava/tasks/task24/Task3233.java
@@ -0,0 +1,9 @@
+package philosophyjava.tasks.task24;
+
+/**
+ * 32. (1) Убедитесь в том, что FixedSizeStack в GenericCast.java генерирует предупреждения при попытке
+ * выхода за границы. Означает ли это, что код проверки границ не обязателен?
+ * 33. (3) Исправьте ошибку в GenericCast.java с использованием ArrayList.
+ */
+public class Task3233 {
+}
